dbf833a2dfb425365ccb31b1fba4f47f91e8e433
MaestracciDamien <damien.maestracci@gmail.com>
2017-03-13 00:45:40 +0100

    login step 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'user/destroy'
 
-  get 'static_pages/help'
+  get  '/help', to: 'static_pages#help'
 
-  get 'static_pages/contact'
+  get  '/contact', to: 'static_pages#contact'
+
 
   resources :users
+  get  '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
 end
