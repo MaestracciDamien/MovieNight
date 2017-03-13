68c59e5eefdd8a66b8d528a110de420cb7d75a04
MaestracciDamien <damien.maestracci@gmail.com>
2017-03-13 23:05:07 +0100

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  root 'users#index'
+
   get  '/help', to: 'static_pages#help'
-
   get  '/contact', to: 'static_pages#contact'
-
+  get  '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :users
-  get  '/signup',  to: 'users#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+
+
 end
 
